751fcaa0665e555d191db0affa2092e4a14bb009
TaniaPerezD <taniamor.perez.d@gmail.com>
2025-02-27 01:48:29 -0400

    boton con mui

---
+++ algoritmos/src/components/BotonAlgoritmos.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Backdrop from '@mui/material/Backdrop';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+
+export default function SpeedDialTooltipOpen({ actions }) {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Box sx={{ height: 330, transform: 'translateZ(0px)', flexGrow: 1 }}>
+      <Backdrop open={open} />
+      <SpeedDial
+        ariaLabel="Algoritmos disponibles"
+        sx={{ position: 'absolute', bottom: 16, right: 16 }}
+        icon={<SpeedDialIcon />}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        open={open}
+      >
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
+            tooltipOpen
+            onClick={() => {
+              action.action(); 
+              handleClose(); 
+            }}
+          />
+        ))}
+      </SpeedDial>
+    </Box>
+  );
+}
